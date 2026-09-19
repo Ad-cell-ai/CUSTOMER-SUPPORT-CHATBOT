5e0d436037f0998eda46b00af6159ae7ce190927
Ad-cell-ai <yadavaadi1108@gmail.com>
2026-09-19 18:44:18 +0530

    commmited

--- backend/chatbot/llm/client.js
+++ backend/chatbot/llm/client.js
@@ -3,5 +3,5 @@ import { GoogleGenAI } from "@google/genai";
 const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY,
 });
-
+console.log("API Key Loaded:", !!process.env.GEMINI_API_KEY);
 export default ai;
