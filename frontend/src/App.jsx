b440bd70a1a4416cd2d42f9ccb26904906d6a898
Ad-cell-ai <yadavaadi1108@gmail.com>
2026-09-19 12:52:19 +0530

    fix errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import "./App.css";
 
 const API_URL =
-  "https://customer-support-chatbot-gf37.onrender.com/api/chat";
+   "https://customer-support-chatbot-gf37.onrender.com/api/chat";
 
 function App() {
   const [message, setMessage] = useState("");
